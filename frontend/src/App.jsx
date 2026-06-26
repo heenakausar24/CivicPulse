f254cdb7026bba19d4c5f403348f268b57a09491
ameys28 <ameysawant2806@gmail.com>
2026-06-27 01:13:44 +0530

    fixed routing issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,12 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 
-function AuthorityLogin() {
-  return <h1>Authority Login</h1>;
-}
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/authority/login" element={<AuthorityLogin />} />
     </Routes>
   );
 }
